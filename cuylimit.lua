19680ca3a51ab335f281a1d65473f1f271ea79e5
Fernando Paredes Garcia <fernando@develcuy.com>
2019-01-06 10:28:45 -0500

    Limit process monitoring to current user only.

--- cuylimit.lua
+++ cuylimit.lua
@@ -3,6 +3,7 @@
 local seawolf = require [[seawolf]].__build([[text]], [[contrib]], [[variable]])
 local trim, xtable = seawolf.text.trim, seawolf.contrib.seawolf_table
 local explode, sleep = seawolf.text.explode, require [[socket]].sleep
+local is_numeric = seawolf.variable.is_numeric
 
 local pattern = arg[1]
 local limit = arg[2]
@@ -23,12 +24,19 @@ function os.capture(cmd)
   return s
 end
 
+-- Detect user invoking this script
+local uid = ((os.capture 'id -u'):gmatch '[^\r\n]+')()
+if not is_numeric(uid) then
+  io.stderr:write "ERROR: Can't detect real user ID (UID)\n"
+  os.exit()
+end
+
 while true do
   local limited = {}
   local procs = {}
 
   -- Get list of running processes
-  for line in (os.capture [[ps axo pid,cmd]] or [[]]):gmatch '[^\r\n]+' do
+  for line in (os.capture(('pgrep -a -u %d "(cpulimit|%s)"'):format(uid, pattern)) or ''):gmatch '[^\r\n]+' do
     line = trim(line)
     local pid, cmd = line:gmatch [[(%d+) (.+)]] ()
 
